dabead85b3cea92e03a2e8ecbf6e5c341207ca83
AleksiJarventausta <aksu.j@hotmail.com>
2021-03-08 19:26:21 +0200

    Add github classroom.

---
+++ test/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export ANDROID_HOME=/users/runner/Library/Android/sdk
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home
+
+
+$ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done;'
+./node_modules/.bin/wdio 
+$ANDROID_HOME/platform-tools/adb -s emulator-5554 emu kill
