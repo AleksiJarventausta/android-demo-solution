88b2f6878e4b54f8a609d77eb599042cdd8c8be6
AleksiJarventausta <aksu.j@hotmail.com>
2021-03-25 10:53:10 +0200

    Change return value

--- test/run.sh
+++ test/run.sh
@@ -6,4 +6,6 @@ export JAVA_HOME=/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/H
 
 $ANDROID_HOME/platform-tools/adb wait-for-device shell 'while [[ -z $(getprop sys.boot_completed) ]]; do sleep 1; done;'
 ./node_modules/.bin/wdio 
+passed=$?
 $ANDROID_HOME/platform-tools/adb -s emulator-5554 emu kill
+return passed
